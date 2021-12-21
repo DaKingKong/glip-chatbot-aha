ec35180d72dee25dd5772accc5e6a46d5b17aaea
da kong <kongda9999@gmail.com>
2021-12-21 13:37:21 +0800

    WIP

--- src/setupDatabase.js
+++ src/setupDatabase.js
@@ -3,6 +3,7 @@ import {Bot, Service} from 'ringcentral-chatbot/dist/models'
 async function setupDatabase (){
     await Bot.sync();
     await Service.sync();
+    console.log("database setup done.")
 }
 
 setupDatabase();
